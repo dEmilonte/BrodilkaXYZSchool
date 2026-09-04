c3469108c157abb1380b3446b8b502830911a35a
dEmilonte <257054481+dEmilonte@users.noreply.github.com>
2026-09-04 14:00:56 +0500

    Initial check-in

---
+++ Assets/Scripts/HERO/Hero.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    private Vector2 _direction;
+
+    public void SetDirection (Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        if (_direction != Vector2.zero)
+        {
+            var deltaX = _direction.x * _speed * Time.deltaTime;
+            var deltaY = _direction.y * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + deltaX;
+            var newYPosition = transform.position.y + deltaY;
+            transform.position = new Vector3 (newXPosition, newYPosition, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/HERO/PlayerInputReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<Vector2>();
+        _hero.SetDirection(direction);
+    }
+
+}
